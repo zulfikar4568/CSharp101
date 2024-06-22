5d06581b74173f9f81a5889d41761800dfdee33b
zulfikar4568 <isnaen70@gmail.com>
2024-06-22 20:12:14 +0700

    perf: oop

--- Basic/07_OOP.cs
+++ Basic/07_OOP.cs
@@ -34,6 +34,33 @@ namespace LearnCSharp.Basic
     
     public string LastName { get; } = lastName;
     public string Address { get; set; } = address;
+
+    public List<Pet> MyPetList = [];
+  }
+
+  abstract class Pet(string name)
+  {
+    public string Name { get; } = name;
+    public abstract void MakeNoise();
+    public void WhoAmI() {
+      Console.WriteLine($"    My name {Name} and I'm {GetType().Name}");
+    }
+  }
+
+  class Dog(string name) : Pet(name)
+  {
+    public override void MakeNoise()
+    {
+      Console.WriteLine("Bark!!!");
+    }
+  }
+
+  class Cat(string name) : Pet(name)
+  {
+    public override void MakeNoise()
+    {
+      Console.WriteLine("Meow!!!");
+    }
   }
 }
 
@@ -48,6 +75,11 @@ namespace LearnCSharp.Basic
 // };
 
 // Person3 person1 = new("Zulfikar", "Isnaen", "St Main Street");
+// Dog dog1 = new("Chiko");
+// Cat cat1 = new("Cilot");
+// person1.MyPetList.Add(dog1);
+// person1.MyPetList.Add(cat1);
+
 // var person2 = new Person3("Sakip", "Hidayat", "St Main Street");
 // // person1.FirstName = "Zul"; // readonly
 
@@ -61,4 +93,9 @@ namespace LearnCSharp.Basic
 // foreach (var person in queryPerson)
 // {
 //   Console.WriteLine($"Hi {person.FirstName} {person.LastName} and my address {person.Address}");
+
+//   foreach (var pet in person.MyPetList)
+//   {
+//     pet.WhoAmI();
+//   }
 // }
