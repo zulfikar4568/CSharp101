4abbd6581880f5fe2b9930b6b7aed49a2a4b4b2c
zulfikar4568 <isnaen70@gmail.com>
2024-06-22 14:28:35 +0700

    perf: looping

---
+++ Basic/04_Looping.cs
@@ -0,0 +1,29 @@
+namespace LearnCSharp.Basic
+{
+  class Looping
+  {
+    static void Execute()
+    {
+      int counter = 0;
+
+      while (counter < 10)
+      {
+        Console.WriteLine($"The counter is {counter}");
+        counter++;
+      }
+
+      counter = 0;
+      do
+      {
+        Console.WriteLine($"The counter is {counter}");
+        counter++;
+      } while (counter < 10);
+
+
+      for (int c = 0; c < 10; c++)
+      {
+        Console.WriteLine($"The counter is {c}");
+      }
+    }
+  }
+}
